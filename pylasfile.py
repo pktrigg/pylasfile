7b1b725a9299d6af2caef00eb7673a40c100271b
pktrigg <p.kennedy@fugro.com>
2017-07-15 19:05:34 +0800

    initial commit

---
+++ pylasfile.py
@@ -0,0 +1,9 @@
+#name:          pylasfile
+#created:       July 2017
+#by:            p.kennedy@fugro.com
+#description:   python module to read and write a ASPRS LAS file natively
+#notes:         See main at end of script for example how to use this
+#based on ASPRS LAS 1.4-R13 15 July 2013
+
+
+# See readme.md for more details
